51232d75a6f072d1e54b6fb6ede419ad0a661823
Mathias Darr <mddarr@gmail.com>
2020-04-14 15:12:14 -0700

    Got the stupid plugin to work

---
+++ avro-movie-kafka/src/main/java/com/mddarr/avromoviekafka/AvroMovieKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.mddarr.avromoviekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroMovieKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvroMovieKafkaApplication.class, args);
+	}
+
+}
---
+++ avro-movie-kafka/src/test/java/com/mddarr/avromoviekafka/AvroMovieKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mddarr.avromoviekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvroMovieKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
